e88669cce39786809360226293e34f0d4dcec0ca
eddogustian <eddogustian78@gmail.com>
2020-07-12 12:06:14 +0700

    crud

---
+++ system/delete.php
@@ -0,0 +1,27 @@
+// koneksi database
+include 'koneksi.php';
+ 
+// menangkap data id yang di kirim dari url
+<?php
+$id = $_GET['id'];
+ 
+ 
+// menghapus data dari database
+mysqli_query($koneksi,"delete from mahasiswa where id='$id'");
+ 
+// mengalihkan halaman kembali ke index.php
+header("location:index.php");
+ 
+?>
+
+
+
+
+
+
+
+
+
+
+
+
